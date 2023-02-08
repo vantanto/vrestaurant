8e06a4c646bfe07eb3d926f6edc57064792256f5
vantanto <kristanto.margojoyo99@gmail.com>
2023-02-09 05:26:35 +0700

    edit: admin nav

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -1,17 +1,14 @@
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
-    <div class="c-sidebar-brand d-lg-down-none">
-        <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="assets/brand/coreui.svg#full"></use>
-        </svg>
-        <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="assets/brand/coreui.svg#signet"></use>
-        </svg>
+    <div class="c-sidebar-brand">
+        {{ config('app.name') }}
     </div>
     <ul class="c-sidebar-nav">
-        <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="index.html">
-                <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="node_modules/@coreui/icons/sprites/free.svg#cil-speedometer"></use>
-                </svg> Dashboard<span class="badge badge-info">NEW</span></a></li>
+        <li class="c-sidebar-nav-item @if(Request::routeIs('dashboard')) c-avtive @endif">
+            <a class="c-sidebar-nav-link" href="{{ route('dashboard') }}">
+                <i class="c-sidebar-nav-icon fa fa-home"></i>
+                Dashboard
+            </a>
+        </li>
         <li class="c-sidebar-nav-title">Theme</li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="colors.html">
                 <svg class="c-sidebar-nav-icon">
